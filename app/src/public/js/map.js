702b3bc84d2feb65f9e35d0f63f57aea51a6e56f
donghyun-git <donghyun1113@hanmail.net>
2022-12-09 22:27:52 +0900

    news api

--- app/src/public/js/map.js
+++ app/src/public/js/map.js
@@ -1,55 +1,33 @@
 //receive data
-
 var newData = [];
+let mapArea = document.getElementById("map");
+let mapTitle = document.querySelector(".map_tit");
+let loadingBar = document.querySelector(".box");
+let markers = new Array();
+let infoWindows = new Array();
 
 async function getData() {
     const res = await fetch('json/Welfare.json');
     const data = await res.json();
     console.log(data);
     
-    let markers = new Array();
-    let infoWindows = new Array();
-    
     for(let i=0; i<data.length; i++){
       naver.maps.Service.fromAddrToCoord({ address: `${data[i].address}` }, function(status, response) {
     if (status === naver.maps.Service.Status.ERROR) {
-    return alert('Something wrong!');
+    return console.log('Something wrong!');
     }
     let lat = response.result.items[0].point.y;
     let lng = response.result.items[0].point.x;
     newData.push({
         name: data[i].name,
         tel: data[i].tel,
+        adr: data[i].address,
         lat: lat,
         lng: lng
     });
     }   
     )  
 }
-    // var infoWindow = new naver.maps.InfoWindow({
-    // content: '<div style="width: 250px; text-align: center; padding:10px;"><b style="font-weight: 300; font-size: 18px;">' + '${json1[i].info.pname} '+ '${json1[i].info.page} '+ '${json1[i].info.sex}' + '<p style="font-size: 16px; font-weight: 400;">' + '${json1[i].info.padr}' + '</p>' + '</b></div>'
-    // });
-
-    // markers.push(marker);
-    // infoWindows.push(infoWindow);
-
-    // function getClickHandler(seq) {
-
-    // return function(e) {  // 마커를 클릭하는 부분
-    //     var marker = markers[seq], // 클릭한 마커의 시퀀스로 찾는다.
-    //         infoWindow = infoWindows[seq]; // 클릭한 마커의 시퀀스로 찾는다
-
-    //     if (infoWindow.getMap()) {
-    //         infoWindow.close();
-    //     } else {
-    //         infoWindow.open(map, marker); // 표출
-    //     }
-    // }
-    // }
-
-    // for (var i=0, j=markers.length; i<j; i++) {
-    // naver.maps.Event.addListener(markers[i], 'click', getClickHandler(i)); // 클릭한 마커 핸들러
-    // }
     
 };
 getData();
@@ -58,17 +36,46 @@ setTimeout(() => {
     console.log(newData);
     var map = new naver.maps.Map('map', {
         center: new naver.maps.LatLng(36.815129, 127.1138939),
-        zoom: 10
+        zoom: 18
         });
     for(var i=0; i<newData.length; i++){
         var marker = new naver.maps.Marker({
                 map: map,
                 position: new naver.maps.LatLng(newData[i].lat, newData[i].lng),
                 });
+        
+        var infoWindow = new naver.maps.InfoWindow({
+            content: '<div class="marker_info">'+'<i class="fa fa-hospital-o" aria-hidden="true"></i>&nbsp <b>' + newData[i].name + '</b><br><h6>' + newData[i].tel +
+            '</h6><br>'+ '<b>'+"주소: "+ '</b>' + newData[i].adr + '</div>',
+            borderWidth: 0,
+            disableAnchor: true,
+            backgroundColor: 'transparent',
+        });
+        
+            markers.push(marker);
+            infoWindows.push(infoWindow);
     }
+    mapTitle.style.display = "inline-block";
+    loadingBar.style.display = "none";
+
+
+        function getClickHandler(seq) {
+    
+        return function(e) {  // 마커를 클릭하는 부분
+            var marker = markers[seq], // 클릭한 마커의 시퀀스로 찾는다.
+                infoWindow = infoWindows[seq]; // 클릭한 마커의 시퀀스로 찾는다
+    
+            if (infoWindow.getMap()) {
+                infoWindow.close();
+            } else {
+                infoWindow.open(map, marker); // 표출
+            }
+        }
+        }
+    
+        for (var i=0, j=markers.length; i<j; i++) {
+        naver.maps.Event.addListener(markers[i], 'click', getClickHandler(i)); // 클릭한 마커 핸들러
+        }
 }, 17000);
 
-setTimeout(() => {
-    
-}, )
 
---
+++ app/src/public/js/new.js
@@ -0,0 +1,111 @@
+let news = [];
+let page = 1;
+let total_page = 0;
+let url;
+let keyword = "노인";
+
+//각 함수에서 필요한 url을 만든다.
+//api 호출 함수를 부른다.
+
+const getNews = async () => {
+  try {
+    let header = new Headers({
+      'x-api-key': '46-vEQGiLyDmpA7w0esNwo0LENLzX9c50w3IopGS2k8'
+    });
+
+    let response = await fetch(url, {
+      headers: header
+    }) //ajax, http, fetch
+    let data = await response.json()
+    console.log(response)
+    console.log(data)
+    if (response.status == 200) {
+      if (data.total_hits == 0) {
+        throw new Error("No match new here!")
+      }
+      news = data.articles;
+      total_pages = data.total_pages;
+      page = data.page;
+      console.log(news)
+      render();
+      pagination();
+    } else {
+      throw new Error(data.message)
+    }
+  } catch (error) {
+    console.log(error.message);
+    errorRender(error.message);
+  }
+}
+
+const getLatestNews = async () => { //비동기 처리, 기존 동기적 언어인 js에서 await을 사용하려면 비동기처리를 선언하는 async를 같이 써주어야 함.
+
+    url = new URL(
+    `https://api.newscatcherapi.com/v2/search?q=${keyword}' &countries=KR&page_size=10`
+  );
+  getNews();
+};
+
+getLatestNews();
+
+
+const getNewsByTopic = async (event) => {
+  console.log("클릭됨", event.target.textContent);
+  let topic = event.target.textContent.toLowerCase();
+  url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&page_size=10&topic=${topic}`);
+  getNews();
+};
+
+
+const render = () => {
+  let resultHTML = '';
+
+  resultHTML = news.map((item) => {
+
+    return `<div class="row news">
+                <div class="col-lg-4">
+                <img src="${item.media == null || item.media == "" ? "img/none-img.jpg" : item.media}">
+                </div>
+                <div class="col-lg-8">
+                    <a href="${item.link}"><h2>${item.title}</h2></a>
+                    <p>
+                    ${item.summary == null || item.summary == "" ? "내용없음" : item.summary > 200 ? item.summary.substring(0,200) + "..." : item.summary}
+                    </p>
+                    <div>
+                    <span>출처 -</span> <strong>${item.rights == null || item.rights == "" ? "no source" : item.rights}</strong> <br>
+                    ${item.published_date || "no source"} <br>
+                    ${moment(item.published_date).fromNow()}
+                    </div>
+                </div>
+                </div>`;
+  }).join('');
+
+  document.getElementById("section").innerHTML = resultHTML;
+};
+
+const errorRender = (message) => {
+  let errorHTML = '';
+  errorHTML = `<div class="alert alert-danger" role="alert">
+    ${message}
+</div>`
+  document.getElementById("section").innerHTML = errorHTML;
+}
+
+const pagination = () => {
+    let paginationHTML = '';
+    //total _page
+    //page
+    //page group
+    let pageGroup = Math.ceil(page/5);
+    //last
+    let last = pageGroup * 5;
+    //first
+    let first = last - 4;
+    //first~last 페이지 프린트
+
+    for(let i=first; i<=last; i++){
+      paginationHTML += `<li class="page-item"><a class="page-link" href="#">${i}</a></li>`
+    }
+
+    document.querySelector(".pagination").innerHTML = paginationHTML;
+}
